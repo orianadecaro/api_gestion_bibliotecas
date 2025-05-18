654096d7639d8a575b2fc86c9ed1f29dc044529c
orianadecaro <orianadecaro@gmail.com>
2025-05-18 16:06:37 -0300

    add new folder

--- api/index.js
+++ api/index.js
@@ -1,31 +1,5 @@
-const express = require("express");
+// api/index.js
 const serverless = require("serverless-http");
-const dotenv = require("dotenv");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../swagger"); // ruta corregida
-const authRoutes = require("../swagger/authRoutes");
-const librosRoutes = require("../swagger/librosRoutes");
-const usuariosRoutes = require("../swagger/usuariosRoutes");
-const sociosRoutes = require("../swagger/sociosRoutes");
-const prestamosRoutes = require("../swagger/prestamosRoutes");
-const perfilesRoutes = require("../swagger/perfilesRoutes");
+const app = require("../index"); // importa tu app express
 
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
-
-// Rutas
-app.use("/", authRoutes);
-app.use("/libros", librosRoutes);
-app.use("/usuarios", usuariosRoutes);
-app.use("/socios", sociosRoutes);
-app.use("/prestamos", prestamosRoutes);
-app.use("/perfiles", perfilesRoutes);
-
-// Documentación Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Exporta como función para Vercel
 module.exports = serverless(app);
---
+++ index.js
@@ -0,0 +1,30 @@
+// app.js
+const express = require("express");
+const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+const authRoutes = require("./swagger/authRoutes");
+const librosRoutes = require("./swagger/librosRoutes");
+const usuariosRoutes = require("./swagger/usuariosRoutes");
+const sociosRoutes = require("./swagger/sociosRoutes");
+const prestamosRoutes = require("./swagger/prestamosRoutes");
+const perfilesRoutes = require("./swagger/perfilesRoutes");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Rutas
+app.use("/", authRoutes);
+app.use("/libros", librosRoutes);
+app.use("/usuarios", usuariosRoutes);
+app.use("/socios", sociosRoutes);
+app.use("/prestamos", prestamosRoutes);
+app.use("/perfiles", perfilesRoutes);
+
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// ❌ NO uses app.listen aquí
+module.exports = app;
