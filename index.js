8897eaf8f020164ed840b9a17126bf38b4495a55
orianadecaro <orianadecaro@gmail.com>
2025-05-20 13:08:16 -0300

    fix

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ app.use("/prestamos", prestamosRoutes);
 app.use("/perfiles", perfilesRoutes);
 
 // Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/api-docs.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
 
 app.get("/", (req, res) => {
   res.send(`
