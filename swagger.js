dcb8203c621588bd3618789ea1512c30b474817d
orianadecaro <orianadecaro@gmail.com>
2025-06-03 18:01:39 -0300

    added auth in global swagger

--- swagger.js
+++ swagger.js
@@ -7,47 +7,47 @@ const options = {
       title: "API de Gestión de Bibliotecas",
       version: "1.0.0",
     },
-  },
-  tags: [
-    {
-      name: "Login",
-      description: "Autenticación de usuarios",
-    },
-    {
-      name: "Libros",
-      description: "Gestión de libros",
-    },
-    {
-      name: "Usuarios",
-      description: "Gestión de usuarios",
-    },
-    {
-      name: "Socios",
-      description: "Gestión de socios",
-    },
-    {
-      name: "Préstamos",
-      description: "Gestión de préstamos",
-    },
-    {
-      name: "Perfiles",
-      description: "Gestión de perfiles",
-    },
-  ],
-  components: {
-    securitySchemes: {
-      bearerAuth: {
-        type: "http",
-        scheme: "bearer",
-        bearerFormat: "JWT",
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT", // Opcional, para que diga "JWT" en Swagger UI
+        },
       },
     },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    tags: [
+      {
+        name: "Login",
+        description: "Autenticación de usuarios",
+      },
+      {
+        name: "Libros",
+        description: "Gestión de libros",
+      },
+      {
+        name: "Usuarios",
+        description: "Gestión de usuarios",
+      },
+      {
+        name: "Socios",
+        description: "Gestión de socios",
+      },
+      {
+        name: "Préstamos",
+        description: "Gestión de préstamos",
+      },
+      {
+        name: "Perfiles",
+        description: "Gestión de perfiles",
+      },
+    ],
   },
-  security: [
-    {
-      bearerAuth: [],
-    },
-  ],
   apis: ["./swagger/*.js"],
 };
 
